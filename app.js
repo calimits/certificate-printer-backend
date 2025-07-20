6b50e5f0331a357e9215f396e057039cfb7e0cdb
calimits <calimitsacd@gmail.com>
2025-07-20 15:09:32 -0400

    api de caca

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({extended: true}));
 
 //Swagger Docs
